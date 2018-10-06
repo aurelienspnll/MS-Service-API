937d4b1d7afc62d8b64380b34820939a74220b5a
Aurélien Spinelli <aurelien.spinelli@hotmail.fr>
2018-10-06 14:04:22 +0200

    Network properties des livraisons

---
+++ services/document/src/main/java/delivery/Network.java
@@ -0,0 +1,20 @@
+package delivery;
+
+import java.util.Properties;
+import java.io.IOException;
+
+public class Network {
+
+    private static String readProperties(String key) {
+        try {
+            Properties prop = new Properties();
+            prop.load(Network.class.getResourceAsStream("/network.properties"));
+            return prop.getProperty(key);
+        } catch(IOException e) { throw new RuntimeException(e); }
+    }
+
+    static final String HOST = readProperties("databaseHostName");
+    static final int PORT = Integer.parseInt(readProperties("databasePort"));
+    static final String DATABASE = "orders";
+    static final String COLLECTION = "delivery";
+}
