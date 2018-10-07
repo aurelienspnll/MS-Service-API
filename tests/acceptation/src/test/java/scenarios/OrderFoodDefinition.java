6a8c93ea2eeae8a984d8891c70f67bef56748f48
mohamed chennouf <mohamed.chennouf@etu.unice.fr>
2018-10-07 16:48:21 +0200

    tests acceptations

--- tests/acceptation/src/test/java/scenarios/OrderFoodDefinition.java
+++ tests/acceptation/src/test/java/scenarios/OrderFoodDefinition.java
@@ -14,6 +14,9 @@ public class OrderFoodDefinition {
     private int port = 9080;
 
     private JSONObject order;
+    private JSONObject answer;
+    private String identifiant;
+    private boolean validate;
 
     private JSONObject call(JSONObject request) {
         String raw =
@@ -32,17 +35,80 @@ public class OrderFoodDefinition {
         assertEquals("done", ans.getString("purge"));
     }
 
-    //order number 44: Hot Wings 30 Bucket at 3 avenue promenade des anglais for Aurélien
-    @Given("^a order with id (.*): (.*) at (.*) for (.*)$")
-    public void orderMeal(String id,String nameMeal, String adress, String clientName) {
+    @Given("^a order with id (.*) added to the registry$")
+    public void preregistered_orderMeal(String id) {
         JSONObject order = new JSONObject();
-        order.put("id", id)
-                    .put("nameOfFood", nameMeal)
-                    .put("nameOfClient", clientName)
-                    .put("addressDestination", adress);
+        order.put("id", id).put("nameOfFood", "wings").put("nameOfClient", "jean").put("addressDestination", "22 promenade des anglais");
         JSONObject ans = call(new JSONObject().put("event", "ORDER").put("orderFood", order));
         assertEquals(true, ans.getBoolean("inserted"));
     }
+
+    @Given("^A order identified as (.*)$")
+    public void initialize_a_order(String id){
+        order = new JSONObject();
+        order.put("id",id);
+    }
+
+    @Given("^key (.*) set to (.*)$")
+    public void add_order_attribute(String key, String value) { order.put(key.trim(),value);  }
+
+
+
+    @When("^the (.*) message is sent$")
+    public void call_registry(String message) {
+        JSONObject request = new JSONObject();
+        switch(message) {
+            case "ORDER":
+                request.put("event", message).put("orderFood", order); break;
+            case "VALIDATE":
+                request.put("event", message).put("id", identifiant).put("validate", validate); break;
+            case "CONSULT":
+                request.put("event", message);
+                break;
+            case "PURGE":
+                request.put("event", message); break;
+            default:
+                throw new RuntimeException("Unknown message");
+        }
+        answer = call(request);
+        assertNotNull(answer);
+    }
+
+    @Then("^the order is registered$")
+    public void the_order_is_registered() {
+        assertEquals(true,answer.getBoolean("inserted"));
+    }
+
+    @Then("^key (.*) is equals to (.*)$")
+    public void check_order_content(String key, String value) {
+        Object data = answer.getJSONObject("orderFood").get(key.trim());
+            assertEquals(value.trim(), data);
+    }
+
+    @Then("^key (.*) is given$")
+    public void check_order_content(String key) {
+        Object data = answer.getJSONObject("orderFood").get(key.trim());
+        assertNotNull(data);
+    }
+
+
+    @Given("^an id identified as (.*)$")
+    public void setIdentifiant(String id) { this.identifiant = id; }
+
+
+    @Given("^set key validate to (.*)$")
+    public void setValidate(String validate) { this.validate = Boolean.parseBoolean(validate); }
+
+    @Then("^order approval is (.*)$")
+    public void the_status(String approved) {
+        assertEquals(Boolean.parseBoolean(approved),answer.getBoolean("approved"));
+    }
+
+    @Then("^there (?:are|is) (\\d+) order(?:s)? in the registry$")
+    public void how_many_orders_in_the_registry(int expected) {
+        assertEquals(answer.getJSONArray("orders").length(),expected);
+    }
+
 
 
 }
