82bad8af3017885dff28d7f84edffe0a497275e9
Aurélien Spinelli <aurelien.spinelli@hotmail.fr>
2018-10-07 18:51:36 +0200

    Acceptation service delivery

--- tests/acceptation/src/test/java/scenarios/DeliveryDefinition.java
+++ tests/acceptation/src/test/java/scenarios/DeliveryDefinition.java
@@ -1,23 +1,28 @@
 package scenarios;
 
-import cucumber.api.java.en.Given;
-import org.apache.cxf.jaxrs.client.WebClient;
+import cucumber.api.java.en.*;
 import org.json.JSONObject;
 
-import static org.junit.Assert.assertEquals;
+import javax.ws.rs.core.MediaType;
+import org.apache.cxf.jaxrs.client.WebClient;
+import static org.junit.Assert.*;
+
 
 public class DeliveryDefinition {
 
     private String host = "localhost";
-    private int port = 8090;
+    private int port = 9100;
 
     private JSONObject delivery;
     private JSONObject answer;
+    private String identifiant;
 
-    private JSONObject callGet() {
+    private JSONObject call(JSONObject request) {
         String raw =
                 WebClient.create("http://" + host + ":" + port + "/delivery-service-document/delivery")
-                        .get(String.class);
+                        .accept(MediaType.APPLICATION_JSON_TYPE)
+                        .header("Content-Type", MediaType.APPLICATION_JSON)
+                        .post(request.toString(), String.class);
         return new JSONObject(raw);
     }
 
@@ -25,17 +30,97 @@ public class DeliveryDefinition {
     public void initialize_port(String host, int port) {
         this.host = host;
         this.port = port;
-        int result = WebClient.create("http://" + host + ":" + port + "/delivery-service-document/delivery").delete().getStatus();
-        assertEquals(200, result);
+        JSONObject ans = call(new JSONObject().put("event", "CLEAN"));
+        assertTrue(ans.getBoolean("allDeleted"));
     }
 
-    @Given("^The delivery (\\d+)$")
-    public void initialize_delivery(String id) { delivery = new JSONObject(); delivery.put("id", id); }
+    @Given("^a delivery with id (.*) added to the registry$")
+    public void preregistered_delivery(String id) {
+        JSONObject delivery = new JSONObject();
+        delivery.put("id", id);
+        delivery.put("deliveryMan", "Escobar");
+        delivery.put("idOrder", "56");
+        delivery.put("delivered", false);
+        JSONObject ans = call(new JSONObject().put("event", "DELIVER").put("delivery", delivery));
+        assertEquals(true, ans.getBoolean("inserted"));
+    }
+
+    @Given("^a delivery with id (\\d+)$")
+    public void initialize_delivery(String id) {
+        delivery = new JSONObject();
+        delivery.put("id", id);
+        delivery.put("delivered", false);
+    }
+
+    @Given("^A delivery identified as (.*)$")
+    public void initialize_a_order(String id){
+        delivery = new JSONObject();
+        delivery.put("id",id);
+        delivery.put("delivered", false);
+    }
+    @Given("^a delivery identified as (\\d+)$")
+    public void a_delivery_identified_as(int id) throws Throwable {
+        delivery = new JSONObject();
+        delivery.put("id",id);
+        delivery.put("delivered", false);
+    }
 
     @Given("^is assigned to (.*)$")
     public void add_deliveryMan(String deliveryMan) { delivery.put("deliveryMan", deliveryMan); }
 
-    @Given("^to deliver the order (\\d+)$")
+
+    @Given("^id order is (\\d+)$")
     public void add_idOrder(String idOrder) { delivery.put("idOrder", idOrder); }
+
+    @When("^the (.*) action is run$")
+    public void call_registry(String message) {
+        JSONObject request = new JSONObject();
+        switch(message) {
+            case "DELIVER":
+                request.put("event", message);
+                request.put("delivery", delivery);
+                break;
+            case "COMPLETE":
+                request.put("event", message);
+                request.put("id", identifiant);
+                break;
+            case "CONSULT":
+                request.put("event", message);
+                request.put("id", identifiant);
+                break;
+            case "LIST":
+                request.put("event", message);
+                break;
+            case "LISTCOMPLETED":
+                request.put("event", message);
+                break;
+            case "LISTNOTCOMPLETED":
+                request.put("event", message);
+                break;
+            case "DELETE":
+                request.put("event", message);
+                request.put("id", identifiant);
+                break;
+            case "CLEAN":
+                request.put("event", message);
+                break;
+
+            default:
+                throw new RuntimeException("Unknown message");
+        }
+        answer = call(request);
+        assertNotNull(answer);
+    }
+
+    @Then("^the delivery is registered$")
+    public void the_delivery_is_registered() {
+        assertEquals(true,answer.getBoolean("inserted"));
+    }
+
+    @Then("^the field (.*) has its value equals to (.*)$")
+    public void check_delivery_content(String key, String value) {
+        Object data = answer.getJSONObject("orderFood").get(key.trim());
+        assertEquals(value.trim(), data);
+    }
 
 }
--- tests/acceptation/src/test/resources/scenarios/delivery.feature
+++ tests/acceptation/src/test/resources/scenarios/delivery.feature
@@ -1,44 +1,15 @@
-Feature: Order food
+Feature: Delivery
 
-  Background:
-    Given an empty orders deployed on localhost:9080
-        And a order with id 1 added to the registry
+    Background:
+        Given an empty delivery deployed on localhost:9100
+        And a delivery with id 42 added to the registry
 
-
-  Scenario: Order a meal
-
-    Given A order identified as 1
-        And key nameOfFood set to wings
-        And key nameOfClient set to jean
-        And key addressDestination set to 22 promenade des anglais
-    When the ORDER message is sent
-    Then the order is registered
-        And key nameOfFood is equals to wings
-        And key nameOfClient is equals to jean
-        And key addressDestination is equals to 22 promenade des anglais
-        And key status is equals to Processing
-        And key deliveryTime is given
-
-
-  Scenario: Validate a order
-    Given an id identified as 1
-        And set key validate to true
-    When the VALIDATE message is sent
-    Then order approval is true
-
-
-  Scenario: Verify that we can consulte all orders
-    Given a order with id 2 added to the registry
-        And a order with id 3 added to the registry
-    When the CONSULT message is sent
-    Then there are 3 orders in the registry
-
-
-
-
-
-
-
-
-
-
+    Scenario: Assign a delevery
+        Given a delivery identified as 56
+            And is assigned to Pablo
+            And id order is 34
+        When the DELIVERY action is run
+        Then the delivery is registered
+            And the field deliveryMan has its value equals to Pablo
+            And the field idOrder has its value equals to 34
+            And the field validated has its value equals to false
