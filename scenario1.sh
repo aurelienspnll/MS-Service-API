b960ac1d94c620c10750e96f05acd490896e6544
mohamed chennouf <mohamed.chennouf@etu.unice.fr>
2018-11-11 18:14:02 +0100

    scénario 1

--- scenario1.sh
+++ scenario1.sh
@@ -71,20 +71,6 @@ curl -X POST -H "Content-Type: application/json" -d "{\"event\": \"VALIDATE\", \
 
 echo ""
 echo ""
-echo "Un event kafka est envoyé chez Delivery"
-
-curl -X POST -H "Content-Type: application/json" -d "{
-  \"event\" : \"DELIVER\",
-  \"delivery\" : {
-    \"deliveryMan\": \"Gail\",
-    \"delivered\": \"false\",
-    \"id\": \"33\",
-    \"idOrder\": \"2\"
-    }
-}" http://localhost:9100/delivery-service-document/delivery
-
-echo ""
-echo ""
 echo "Un event kafka est envoyé chez Bank"
 
 curl -X POST -H "Content-Type: application/json" -d "{
@@ -111,4 +97,83 @@ echo ""
 echo "Jordan le chef du restaurant consulte ces commandes"
 echo "Press any key to continue..."
 read
+curl -X POST -H "Content-Type: application/json" -d "{
+  \"event\": \"CONSULT\"
+}" http://localhost:9080/order-service-document/ordersFood
+
+echo ""
+echo ""
+echo "Jordan Prépare le plat et ajoute au service Delevery"
+echo "Press any key to continue..."
+read
+
+
+
+curl -X POST -H "Content-Type: application/json" -d "{\"event\" : \"DELIVER\",
+         \"delivery\" : {
+            \"id\": \"12\",
+            \"order\": {
+            	\"idOrder\" : \"2\",
+            	\"idClient\" : \"65\",
+            	\"clientName\" : \"Gail\",
+            	\"address\" : \"Rue de Medeline\"
+
+            },
+            \"deliveryMan\": {
+            	\"idDeliveryMan\" : \"2\",
+            	\"firstName\": \"Tomas\",
+            	\"lastName\" : \"Escobar\"
+            },
+            \"delivered\": false
+         }
+}" http://localhost:9100/delivery-service-document/delivery
+
+echo ""
+echo ""
+echo "Jormie est coursier est souhaite consulter les commandes non livré"
+echo "Press any key to continue..."
+read
+
+curl -X POST -H "Content-Type: application/json" -d {"event": "LISTNOTCOMPLETED"} http://localhost:9100/delivery-service-document/delivery
+
+
+echo ""
+echo ""
+echo "Jormie veut maintenant consulter les pret de sa position"
+echo "Press any key to continue..."
+read
+
+curl -X POST -H "Content-Type: application/json" -d "{\"deliveries\": [
+{\"id\":2, posX:22, posY:122, posrX:122, posrY:23},
+{\"id\":2, posX:42, posY:152, posrX:12, posrY:13},
+{\"id\":2, posX:2, posY:52, posrX:2, posrY:13}
+] }" http://localhost:9110/around-service-rpc/CalculateKilometre
+
+echo ""
+echo ""
+echo "Jormie prend une commande et la fait"
+echo "Press any key to continue..."
+read
+
+curl -X POST -H "Content-Type: application/json" -d "{\"event\": \"COMPLETE\", \"id\":\"6\"}" http://localhost:9100/delivery-service-document/delivery
+
+echo ""
+echo ""
+echo "Un event kafka est envoyé à la Bank"
+
+curl -X POST -H "Content-Type: application/json" -d "{
+  \"event\" : \"ADDPAYMENT\",
+  \"payment\" : {
+    \"price\": \"20\",
+    \"id\": \"Jamie\"
+    \"type\": \"virement\"
+    }
+}" http://localhost:9120/bank-service-document/payment
+
+echo ""
+echo ""
+echo "Jamie est payé"
+echo "Gail a recu sa commande"
+
+
 
--- services/around/src/main/java/resource/CalculateKilometreService.java
+++ services/around/src/main/java/resource/CalculateKilometreService.java
@@ -1,5 +1,6 @@
 package resource;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -16,8 +17,20 @@ public class CalculateKilometreService {
         int min = 5;
         int max = 100;
         double delivery = min + rand.nextInt(max - min + 1);
+        JSONArray array = new JSONArray();
+        JSONObject response1 = new JSONObject();
+        response1.put("idDelivery","33");
+        response1.put("distance",delivery+"km");
+
+        JSONObject response2 = new JSONObject();
+        response2.put("idDelivery","2");
+        response2.put("distance","1km");
+
+        array.put(response1);
+        array.put(response2);
+
         try {
-            return Response.ok().entity(delivery+" km").build();
+            return Response.ok().entity(array).build();
         }catch(Exception e) {
             JSONObject error = new JSONObject().put("error", e.toString());
             return Response.status(400).entity(error.toString()).build();
